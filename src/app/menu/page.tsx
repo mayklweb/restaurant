b9ae3a19123b3b5c812759935ebce128b86eec4d
mayklweb <muhammadevvv@gmail.com>
2025-09-05 12:11:10 +0500

    m

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -21,8 +21,8 @@ function Menu() {
                   <Image
                     src="https://static.wixstatic.com/media/a40f43_5c408c6ce8b84b3a8d8eda1ce1c99ce1~mv2.jpg/v1/fill/w_520,h_520,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/a40f43_5c408c6ce8b84b3a8d8eda1ce1c99ce1~mv2.jpg"
                     alt="menu1"
-                    width={500}
-                    height={500}
+                    width={520}
+                    height={520}
                     className="w-full h-auto object-cover"
                   />
                 </div>
@@ -35,10 +35,10 @@ function Menu() {
               <div>
                 <div>
                   <Image
-                    src="https://static.wixstatic.com/media/a40f43_5c408c6ce8b84b3a8d8eda1ce1c99ce1~mv2.jpg/v1/fill/w_520,h_520,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/a40f43_5c408c6ce8b84b3a8d8eda1ce1c99ce1~mv2.jpg"
+                    src="https://static.wixstatic.com/media/a40f43_9284af6c0f374761ae81ed0d28cba43e~mv2.jpg/v1/fill/w_836,h_836,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/a40f43_9284af6c0f374761ae81ed0d28cba43e~mv2.jpg"
                     alt="menu1"
-                    width={500}
-                    height={500}
+                    width={520}
+                    height={520}
                     className="w-full h-auto object-cover"
                   />
                 </div>
@@ -51,10 +51,10 @@ function Menu() {
               <div>
                 <div>
                   <Image
-                    src="https://static.wixstatic.com/media/a40f43_5c408c6ce8b84b3a8d8eda1ce1c99ce1~mv2.jpg/v1/fill/w_520,h_520,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/a40f43_5c408c6ce8b84b3a8d8eda1ce1c99ce1~mv2.jpg"
+                    src="https://static.wixstatic.com/media/a40f43_e28eea9c177642ecaab36ae53c43397b~mv2.jpg/v1/fill/w_836,h_836,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/a40f43_e28eea9c177642ecaab36ae53c43397b~mv2.jpg"
                     alt="menu1"
-                    width={500}
-                    height={500}
+                    width={520}
+                    height={520}
                     className="w-full h-auto object-cover"
                   />
                 </div>
@@ -67,15 +67,15 @@ function Menu() {
               <div>
                 <div>
                   <Image
-                    src="https://static.wixstatic.com/media/a40f43_5c408c6ce8b84b3a8d8eda1ce1c99ce1~mv2.jpg/v1/fill/w_520,h_520,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/a40f43_5c408c6ce8b84b3a8d8eda1ce1c99ce1~mv2.jpg"
+                    src="https://static.wixstatic.com/media/a40f43_998669914d3645aaa0d8084482624796~mv2.jpg/v1/fill/w_836,h_836,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/a40f43_998669914d3645aaa0d8084482624796~mv2.jpg"
                     alt="menu1"
-                    width={500}
-                    height={500}
+                    width={520}
+                    height={520}
                     className="w-full h-auto object-cover"
                   />
                 </div>
                 <div className="mt-5">
-                  <h5 className="text-lg text-black font-semibold">YARRA SPRITZ</h5>
+                  <h5 className="text-lg text-black font-semibold">MATCHA-</h5>
                   <p className="mt-2.5 text-xs text-[#979797]">Strawberry Gum Aperol / Lemon Balm Vodka / Strawberry Sparkling Wine</p>
                   <p className="mt-2.5 text-xs text-[#979797]">THB 360</p>
                 </div>
